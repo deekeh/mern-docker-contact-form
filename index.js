ac0d053aff14944daecf6790c4a04e33831c2a98
DK <darshankhot@programmer.net>
2020-11-06 13:14:46 +0530

    added debug package, helmet package to change document title, docker volumes for persistent data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
+const dev = require('debug')('dev'), test = require('debug')('test')
 const path = require('path')
 const express = require('express')
 var MongoClient = require('mongodb').MongoClient
 const app = express();
 
-var bodyParser = require('body-parser')
 app.use(express.json({ limit: '1mb' }));
 
 app.use(express.static(path.join(__dirname, 'build')))
@@ -18,15 +18,15 @@ app.post('/contact-form', (req, res) => {
         title: req.body.title,
         message: req.body.message
     };
-    console.log(formData);
+    test(formData);
 
     MongoClient.connect(url, (err, client) => {
-        if (err) return console.log(err)
+        if (err) return dev(err)
         const db = client.db(dbName);
         const collection = db.collection('responses');
         collection.insertOne(formData, (err, result) => {
-            if (err) return console.log(err);
-            console.log('Inserted successfully');
+            if (err) return dev(err);
+            dev('Inserted successfully');
         })
     })
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Footer, Header } from './components/Parts'
 import ContactForm from './components/ContactForm'
 import './style.css'
+import {Helmet} from 'react-helmet'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>
+          DK - Contact Form
+        </title>
+      </Helmet>
       <Header />
       <ContactForm></ContactForm>
       <Footer />
